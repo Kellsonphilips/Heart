5bb428895f3972666e64825b09498fe592880209
kellsonphilips <kellsonphilips@gmail.com>
2025-06-18 09:48:17 +0100

    input

---
+++ main.py
@@ -0,0 +1,17 @@
+
+from turtle import *
+
+hideturtle()
+color("red")
+begin_fill()
+pensize(5)
+left(50)
+forward(133)
+circle(50, 200)
+right(140)
+circle(50, 200)
+forward(133)
+textinput("Luv from Me", "I Love U")
+end_fill()
+
+exitonclick()
